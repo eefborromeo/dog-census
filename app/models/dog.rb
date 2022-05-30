1d2efd895875343bf37af0bcda1bebbfbec6c9c2
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-05-30 12:10:10 +0800

    adds validation for likes and dislikes

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,5 +1,7 @@
 class Dog < ApplicationRecord
-    validates :name, presence true, length: { minimum: 1}
-    validates :age, presence true
-    validates :breed, presence true, length: { minimum: 2}
+    validates :name, presence: true, length: { minimum: 1 }
+    validates :age, presence: true
+    validates :breed, presence: true, length: { minimum: 2 }
+    validates :likes, length: { maximum: 200 }
+    validates :dislikes, length: { maximum: 200 }
 end
