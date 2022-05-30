4c0b1927f5886f1ff93c456a45407a6722b6e1b1
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-05-30 11:43:15 +0800

    adds validation

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,5 @@
 class Dog < ApplicationRecord
+    validates :name, presence true, length: { minimum: 1}
+    validates :age, presence true
+    validates :breed, presence true, length: { minimum: 2}
 end
