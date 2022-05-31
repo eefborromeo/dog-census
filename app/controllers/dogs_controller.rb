c9df2b5d38570688168dda364a6b3e00aab01326
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-05-31 19:36:17 +0800

    add edit, update, destroy

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,10 +1,11 @@
 class DogsController < ApplicationController
+    before_action :set_dog, only: [:show, :edit, :update, :destroy]
+    
     def index
         @dogs = Dog.all.order(created_at: :desc)
     end
 
     def show
-        @dog = Dog.find(params[:id])
     end
 
     def new
@@ -12,10 +13,36 @@ class DogsController < ApplicationController
     end
 
     def create
-        @dog = Dog.new()
+        @dog = Dog.new(dog_params)
+
+        if @dog.save
+            redirect_to dogs_path
+        else
+            render :new
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @dog.update(dog_params)
+            redirect_to @dog
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @dog.destroy
+        redirect_to dogs_path
     end
 
     private
+
+        def set_dog
+            @dog = Dog.find(params[:id])
+        end
 
         def dog_params
             params.require(:dog).permit(:name, :age, :breed, :likes, :dislikes)
