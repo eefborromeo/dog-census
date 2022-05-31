5214e4ab6955da1ca77d40cbe15ef9da1e23bc4a
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-05-31 13:32:48 +0800

    added styling for index and show page

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,9 +1,23 @@
 class DogsController < ApplicationController
     def index
-        @dogs = Dog.all
+        @dogs = Dog.all.order(created_at: :desc)
     end
 
     def show
         @dog = Dog.find(params[:id])
     end
+
+    def new
+        @dog = Dog.new
+    end
+
+    def create
+        @dog = Dog.new()
+    end
+
+    private
+
+        def dog_params
+            params.require(:dog).permit(:name, :age, :breed, :likes, :dislikes)
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/dogs', to: 'dogs#index'
+  get '/dogs', to: 'dogs#index', as: 'dogs'
+  get '/dogs/new', to: 'dogs#new'
   get '/dogs/:id', to: 'dogs#show'
+  post '/dogs', to: 'dogs#create'
 end
