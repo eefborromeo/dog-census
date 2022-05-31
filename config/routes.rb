b1da7bafda8f0278ce59d5e1c5d9d82516bb937f
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-05-31 21:30:00 +0800

    move root route to bottom of file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'dogs#index'
-  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html 
   get '/dogs', to: 'dogs#index', as: 'dogs'
   get '/dogs/new', to: 'dogs#new'
   get '/dogs/:id/edit', to: 'dogs#edit', as: 'edit_dogs'
@@ -10,4 +8,5 @@ Rails.application.routes.draw do
   patch '/dogs/:id', to: 'dogs#update'
   delete '/dogs/:id', to: 'dogs#destroy' 
   
+  root 'dogs#index'
 end
