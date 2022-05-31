89da74671aa223829b3963c9523cb2e0f687d447
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-05-31 19:35:24 +0800

    create routes for root, edit, patch, delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'dogs#index'
+  
   get '/dogs', to: 'dogs#index', as: 'dogs'
   get '/dogs/new', to: 'dogs#new'
-  get '/dogs/:id', to: 'dogs#show'
+  get '/dogs/:id/edit', to: 'dogs#edit', as: 'edit_dogs'
+  get '/dogs/:id', to: 'dogs#show', as: 'dog'
   post '/dogs', to: 'dogs#create'
+  patch '/dogs/:id', to: 'dogs#update'
+  delete '/dogs/:id', to: 'dogs#destroy' 
+  
 end
