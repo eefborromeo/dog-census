5f998714cf50c36097fe3aad4467f20b2e8a0aee
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-06-01 23:36:34 +0800

    adjusts update image to go through

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -8,13 +8,15 @@ class Dog < ApplicationRecord
     validate :acceptable_image
 
     def acceptable_image
-        unless avatar.byte_size <= 5.megabyte
-            errors.add(:avatar, "is too big")
-        end
+        if avatar.attached?
+            unless avatar.byte_size <= 5.megabyte
+                errors.add(:avatar, "is too big")
+            end
 
-        acceptable_types = ["image/jpeg", "image/png"]
-        unless acceptable_types.include?(avatar.content_type)
-            erros.add(:avatar, "must be a JPEG or PNG")
+            acceptable_types = ["image/jpeg", "image/png"]
+            unless acceptable_types.include?(avatar.content_type)
+                erros.add(:avatar, "must be a JPEG or PNG")
+            end
         end
     end
 end
